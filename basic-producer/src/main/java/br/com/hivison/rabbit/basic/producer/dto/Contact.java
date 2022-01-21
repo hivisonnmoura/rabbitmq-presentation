2c37d67dc0fbf22e9dae84fbde257f10061e123b
Hivison Moura <hivison.moura@gmail.com>
2022-01-21 18:08:56 -0300

    Changing contact entity to use LocalDate instead of LocalDataTime

--- basic-producer/src/main/java/br/com/hivison/rabbit/basic/producer/dto/Contact.java
+++ basic-producer/src/main/java/br/com/hivison/rabbit/basic/producer/dto/Contact.java
@@ -1,14 +1,14 @@
 package br.com.hivison.rabbit.basic.producer.dto;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @JsonSerialize
 public record Contact(
         String name,
-        @JsonSerialize(using = LocalDateTimeSerializer.class) LocalDateTime  birthday,
+        @JsonSerialize(using = LocalDateSerializer.class) LocalDate birthday,
         String phoneNumber
 ) {
 }
